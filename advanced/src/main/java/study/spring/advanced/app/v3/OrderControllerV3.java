7dd403c4c55618056909c420f6a4d99682276465
YoungChulShin <go1323@gmail.com>
2022-03-01 23:09:13 +0900

    app v3 추가

---
+++ advanced/src/main/java/study/spring/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,30 @@
+package study.spring.advanced.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.spring.advanced.trace.TraceStatus;
+import study.spring.advanced.trace.hellotrace.HelloTraceV2;
+import study.spring.advanced.trace.logtrace.LogTrace;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+  private final OrderServiceV3 orderService;
+  private final LogTrace trace;
+
+  @GetMapping("/v3/request")
+  public String request(String itemId) {
+    TraceStatus status = null;
+    try {
+      status = trace.begin("OrderController.request");
+      orderService.orderItem(itemId);
+      trace.end(status);
+      return "ok";
+    } catch (Exception e) {
+      trace.exception(status, e);
+      throw e;
+    }
+  }
+}
