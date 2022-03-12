7953af77394fa35348b9ece49b64f2138329ed2d
YoungChulShin <go1323@gmail.com>
2022-03-12 23:13:05 +0900

    LogTrace 구현체 변경

--- advanced/src/main/java/study/spring/advanced/LogTraceConfig.java
+++ advanced/src/main/java/study/spring/advanced/LogTraceConfig.java
@@ -2,15 +2,15 @@ package study.spring.advanced;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import study.spring.advanced.trace.logtrace.FieldLogTrace;
 import study.spring.advanced.trace.logtrace.LogTrace;
+import study.spring.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
   @Bean
   public LogTrace logTrace() {
-    return new FieldLogTrace();
+    return new ThreadLocalLogTrace();
   }
 
 }
