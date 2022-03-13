40992c8c45ac17c67819cf21a22b807fc34be393
YoungChulShin <go1323@gmail.com>
2022-03-13 16:09:59 +0900

    abstractTemplate 테스트코드 추가

--- advanced/src/test/java/study/spring/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/study/spring/advanced/trace/template/TemplateMethodTest.java
@@ -2,6 +2,9 @@ package study.spring.advanced.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import study.spring.advanced.trace.template.code.AbstractTemplate;
+import study.spring.advanced.trace.template.code.SubClassLogic1;
+import study.spring.advanced.trace.template.code.SubClassLogic2;
 
 @Slf4j
 public class TemplateMethodTest {
@@ -30,5 +33,30 @@ public class TemplateMethodTest {
     long endTime = System.currentTimeMillis();
     long resultTime = endTime - startTime;
     log.info("resultTime={}", resultTime);
+  }
+
+  @Test
+  void templateMethodV1() {
+    AbstractTemplate abstractTemplate = new SubClassLogic1();
+    abstractTemplate.execute();
+  }
+
+  @Test
+  void templateMethodV2() {
+    AbstractTemplate abstractTemplate = new SubClassLogic2();
+    abstractTemplate.execute();
+  }
+
+  @Test
+  void templateMethodV3() {
+    AbstractTemplate abstractTemplate = new AbstractTemplate() {
+      @Override
+      protected void call() {
+        log.info("비지늣 로직 3실행");
+      }
+    };
+    // 클래스를 임의로 만들어 준다. $를 붙여준다.
+    log.info("클래스 이름 = {}", abstractTemplate.getClass());
+    abstractTemplate.execute();
   }
 }
