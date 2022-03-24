2134548fbcf6a83dc66da2d9ea0d88ab24b5b2b8
YoungChulShin <go1323@gmail.com>
2022-03-24 22:36:31 +0900

    proxyFactory - pointcut 생성

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,8 +3,12 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -24,6 +28,58 @@ public class AdvisorTest {
     log.info("classType = {}", proxy.getClass());
     proxy.save();
     proxy.find();
+  }
+
+  @Test
+  @DisplayName("직접 만든 포인트컷")
+  void advisorTest2() {
+    ServiceInterface target = new ServiceImpl();
+    ProxyFactory proxyFactory = new ProxyFactory(target);
+    DefaultPointcutAdvisor advisor =
+        new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+    proxyFactory.addAdvisor(advisor);
+
+    ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+    log.info("classType = {}", proxy.getClass());
+    proxy.save();
+    proxy.find();
+  }
+
+  static class MyPointcut implements Pointcut {
+
+    @Override
+    public ClassFilter getClassFilter() {
+      return ClassFilter.TRUE;
+    }
+
+    @Override
+    public MethodMatcher getMethodMatcher() {
+      return new MyMethodMatcher();
+    }
+  }
+
+  @Slf4j
+  static class MyMethodMatcher implements MethodMatcher {
+
+    private final String matchName = "save";
+
+    @Override
+    public boolean matches(Method method, Class<?> targetClass) {
+      boolean result = method.getName().equals(matchName);
+      log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+      log.info("포인트컷 결과 result={}", result);
+      return result;
+    }
+
+    @Override
+    public boolean isRuntime() {
+      return false;
+    }
+
+    @Override
+    public boolean matches(Method method, Class<?> targetClass, Object... args) {
+      return false;
+    }
   }
 
 }
